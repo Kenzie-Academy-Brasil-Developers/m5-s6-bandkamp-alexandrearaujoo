5ee9f17349365366f0fe3f93087ca66049c1a3a8
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-05 09:47:35 -0300

    feat: drf-spectacular config done

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -65,7 +65,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'musicians',
     'songs',
-    'albums'
+    'albums',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -154,3 +155,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Band Kamp API',
+    'DESCRIPTION': 'Api for registering musicians, albums and songs',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,  
+}
