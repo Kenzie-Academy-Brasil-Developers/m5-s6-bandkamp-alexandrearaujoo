92af1aca50942770dd5a221099397455cc8356ca
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-06 11:50:37 -0300

    heroku config
    Signed-off-by: Alexandre Araujo <alexandre01393@gmail.com>

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -23,7 +23,7 @@ if DATABASE_URL:
     db_from_env = dj_database_url.config(
         default=DATABASE_URL, conn_max_age=500, ssl_require=True
     )
-    DATABASES['default'].update(db_from_env)
+    DATABASE_URL['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
