f3d2ab163ee5af1c30b2e135da11200001a947d6
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-07 09:53:00 -0300

    heroku config
    Signed-off-by: Alexandre Araujo <alexandre01393@gmail.com>

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -19,13 +19,6 @@ dotenv.load_dotenv()
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if DATABASE_URL:
-    db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True
-    )
-    DATABASES = {"default": {**db_from_env}}
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,7 +39,14 @@ else:
         "HOST": "127.0.0.1",
         "PORT": os.getenv('POSTGRES_PORT')
     }
+
 }
+
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+    )
+    DATABASES["default"].update(db_from_env)
 
 
 
@@ -59,7 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api-bandkamp.herokuapp.com", "*"]
 
 
 # Application definition
